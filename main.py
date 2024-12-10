484b6d371572b6b6e22fe001552d7da3087db6cf
TrueBuch <theduak.dev@gmail.com>
2024-12-11 01:13:39 +0500

    Меню, энд гейм, рестарт игры

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import sys
 
 game.init()
 
+
+sys_font = game.font.SysFont('arial', 64)
+
 #Размеры окна
 screen_x = 500
 screen_y = 500
@@ -11,29 +14,31 @@ screen_y = 500
 zone_size = 100 #Размер зоны для крестика/нолика
 
 screen = None
-game_status = 'running'
+running = True
+game_status = 'menu'
+winner = None
 mouse_pressed = False
 
-#Загрузка изображений
+#Загрузка спрайтов
 field = game.image.load('sprites/field.png')
 Nought = game.image.load('sprites/Nought.png')
 Cross = game.image.load('sprites/Cross.png')
 
-overlay = game.Surface((zone_size, zone_size), game.SRCALPHA) #Прозрачная поверхность для зон с размером 100
+zone_overlay = game.Surface((zone_size, zone_size), game.SRCALPHA) #Прозрачная поверхность для зон с размером 100
+game.draw.rect(zone_overlay, (0, 0, 0, 64), (0, 0, zone_size, zone_size), border_radius=27) #Полупрозрачаня поверхность с закругленными краями
 
+start_button_overlay = game.Surface((300, 450), game.SRCALPHA) #Прозрачная поверхность
+start_button_overlay_rect = start_button_overlay.get_rect(center = (screen_x // 2, screen_y / 2))
+game.draw.rect(start_button_overlay, (0, 0, 0, 64), (0, 0, 300, 450), border_radius=27)
 
 screen = game.display.set_mode((screen_x, screen_y)) #Создание экрана
 
 field_rect = field.get_rect(center = (screen_x // 2, screen_y / 2)) #Получение объекта класса Rect, с местоположением в центре экрана
-game.draw.rect(overlay, (0, 0, 0, 64), (0, 0, zone_size, zone_size), border_radius=27) #Полупрозрачаня поверхность с закругленными краями
 
-# !!!
-# !!!
-#[БУДЕТ ИЗМЕНЕНО] Очередь хода
-# True = X 
-# False = O
-# !!!
-# !!!
+
+#Ход игрока
+#True = X
+#False = O
 Turn = True
 
 
@@ -61,6 +66,7 @@ for id, offset in enumerate(offsets):
 def check_win():  # Проверка на победу
     global zones
     global wins
+    global winner
     posX = []
     posO = []
     counter = 0
@@ -76,14 +82,14 @@ def check_win():  # Проверка на победу
             counter += 1
     for i in wins:
         if all([j in posX for j in i]):
-            print('победа X')
-            return 'X'
+            winner = 'Победа X'
+            return 'end_game'
         if all([j in posO for j in i]):
-            print('победа О')
-            return 'O'
+            winner = 'Победа O'
+            return 'end_game'
     if counter == 9:
-        print('Ничья')
-        return 'Tie'
+        winner = 'Ничья'
+        return 'end_game'
     return 'running'
 
 # def main():  # Основная функция
@@ -138,48 +144,72 @@ def check_win():  # Проверка на победу
 #         check_field(game_field, int(input()), playero)
 # main()
 
-while game_status == 'running':
-    game_status = check_win()
-    
-    screen.fill((0, 154, 100)) #Заполнение экрана цветов
-
-    screen.blit(field, field_rect) #Отрисовка игрового поля в центре экрана
-
+while running:
+    print(game_status)
     mouse_pos = game.mouse.get_pos() #Координаты мыши
     # print(mouse_pos)
 
+    screen.fill((0, 154, 100)) #Заполнение экрана цветов
 
-    for id in zones:
-        zone = zones.get(id)
 
-        # Отрисовка крестиков и ноликов, если они имеются на поле
-        if zone[1] == 'X':
-            screen.blit(Cross, zone[0].topleft)
-        if zone[1] == 'O':
-            screen.blit(Nought, zone[0].topleft)
-
-        #Обработка наведения мыши на зону, куда можно поставить крестик/нолик
-        if zone[0].collidepoint(mouse_pos) and zone[1] == 'Null':
-            screen.blit(overlay, zone[0].topleft)
-            #!!!
-            #!!!
-            #[БУДЕТ ИЗМЕНЕНО] Обработка нажатия кнопки на зону, установка крестика или нолика
-            #!!!
-            #!!!
-            if game.mouse.get_pressed()[0] == True and mouse_pressed == False:
-                if Turn:
-                    zone[1] = 'X'
-                else: 
-                    zone[1] = 'O'
-                Turn = not(Turn)
-                mouse_pressed = True
+    if game_status == 'menu':
         
-        if game.mouse.get_pressed()[2] == True:
-            Turn = True
-            zone[1] = 'Null'
+        
+        screen.blit(start_button_overlay, start_button_overlay_rect)
+        
+        if game.mouse.get_pressed()[0] == True and start_button_overlay_rect.collidepoint(mouse_pos):
+            game_status = 'running'
+            mouse_pressed = True
+        
 
+    if game_status == 'running':
+        game_status = check_win()
+        screen.blit(field, field_rect) #Отрисовка игрового поля в центре экрана
+        for id in zones:
+            zone = zones.get(id)
+
+            # Отрисовка крестиков и ноликов, если они имеются на поле
+            if zone[1] == 'X':
+                screen.blit(Cross, zone[0].topleft)
+            if zone[1] == 'O':
+                screen.blit(Nought, zone[0].topleft)
+
+            #Обработка наведения мыши на зону, куда можно поставить крестик/нолик
+            if zone[0].collidepoint(mouse_pos) and zone[1] == 'Null':
+                screen.blit(zone_overlay, zone[0].topleft)
+                #!!!
+                #!!!
+                #[БУДЕТ ИЗМЕНЕНО] Обработка нажатия кнопки на зону, установка крестика или нолика
+                #!!!
+                #!!!
+                if game.mouse.get_pressed()[0] == True and mouse_pressed == False:
+                    if Turn:
+                        zone[1] = 'X'
+                    else: 
+                        zone[1] = 'O'
+                    Turn = not(Turn)
+                    mouse_pressed = True
+            
+            if game.mouse.get_pressed()[2] == True:
+                Turn = True
+                zone[1] = 'Null'
+
+    
+    if game_status == 'end_game':
+        screen.blit(start_button_overlay, start_button_overlay_rect)
+        sys_text = sys_font.render(winner, 1, (255, 0, 0))
+        sys_text_rect = sys_text.get_rect(center=(screen_x // 2, screen_y // 2 - 150))
+        screen.blit(sys_text, sys_text_rect)
+        if game.mouse.get_pressed()[0] == True and start_button_overlay_rect.collidepoint(mouse_pos) and mouse_pressed == False:
+            for id in zones:
+                zone = zones.get(id)
+                zone[1] = 'Null'
+            Turn = True
+            game_status = 'running'
+            mouse_pressed = True
+    
     if game.mouse.get_pressed()[0] == False:
-        mouse_pressed = False
+            mouse_pressed = False
         
     #Обработка события закрытия игры
     for event in game.event.get():
